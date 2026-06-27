9eaaa972190261234780f66e64d7c9d8ac00bb29
ameya3006 <ameyadeshpande15@gmail.com>
2026-06-27 18:43:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,13 +9,11 @@ let current = 0;
 let isPlaying = false;
 let ended = false;
 
-// ➕ Add video
 app.post("/add", (req, res) => {
   playlist.push(req.body.url);
   res.sendStatus(200);
 });
 
-// ▶ Start playlist
 app.post("/start", (req, res) => {
   current = 0;
   isPlaying = true;
@@ -23,19 +21,16 @@ app.post("/start", (req, res) => {
   res.sendStatus(200);
 });
 
-// ▶ Play
 app.post("/play", (req, res) => {
   isPlaying = true;
   res.sendStatus(200);
 });
 
-// ⏸ Pause
 app.post("/pause", (req, res) => {
   isPlaying = false;
   res.sendStatus(200);
 });
 
-// ⏭ Next
 app.post("/next", (req, res) => {
   if (playlist.length > 0) {
     current = (current + 1) % playlist.length;
@@ -44,7 +39,6 @@ app.post("/next", (req, res) => {
   res.sendStatus(200);
 });
 
-// 📺 Current video
 app.get("/current", (req, res) => {
   res.json({
     url: playlist[current] || "",
@@ -52,19 +46,16 @@ app.get("/current", (req, res) => {
   });
 });
 
-// 📜 Playlist
 app.get("/playlist", (req, res) => {
   res.json(playlist);
 });
 
-// 🔥 Ended
 app.post("/ended", (req, res) => {
   ended = true;
   isPlaying = false;
   res.sendStatus(200);
 });
 
-// 📊 Status
 app.get("/status", (req, res) => {
   res.json({ ended, isPlaying });
 });

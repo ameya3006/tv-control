a3096daea40be554eebf18c9675d1a5044055230
ameya3006 <ameyadeshpande15@gmail.com>
2026-06-28 18:13:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,9 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-
 app.use(express.json());
-app.use(express.static(__dirname)); // VERY IMPORTANT
+app.use(express.static(__dirname));
 
-// home = control.html
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "control.html"));
-});
-
-// state + APIs (same as before)
 let playlist = [];
 let current = 0;
 
@@ -20,15 +13,24 @@ let state = {
   isPlaying: false
 };
 
+// main page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "control.html"));
+});
+
+// state
 app.get("/state", (req, res) => res.json(state));
 
+// add video
 app.post("/add", (req, res) => {
   playlist.push(req.body.url);
   res.sendStatus(200);
 });
 
+// playlist
 app.get("/playlist", (req, res) => res.json(playlist));
 
+// controls
 app.post("/start", (req, res) => {
   if (playlist.length > 0) {
     current = 0;

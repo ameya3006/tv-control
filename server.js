96c9212ab9119eb9c831a27f14c6334e7ce36bd0
ameya3006 <ameyadeshpande15@gmail.com>
2026-06-29 10:16:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,41 +6,53 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-let current = {
-  url: "",
-  playing: false
-};
+let playlist = [];
+let currentIndex = -1;
+let playing = false;
 
+// 📺 get current video
 app.get("/video", (req, res) => {
-  res.json(current);
+  res.json({
+    url: playlist[currentIndex] || "",
+    playing: playing
+  });
 });
 
-app.post("/set", (req, res) => {
-  current.url = req.body.url;
-  current.playing = false;
+// ➕ add video
+app.post("/add", (req, res) => {
+  playlist.push(req.body.url);
   res.sendStatus(200);
 });
 
+// ▶ start first
 app.post("/start", (req, res) => {
-  current.playing = true;
+  if (playlist.length > 0) {
+    currentIndex = 0;
+    playing = true;
+  }
   res.sendStatus(200);
 });
 
-app.post("/resume", (req, res) => {
-  current.playing = true;
-  res.sendStatus(200);
-});
-
+// ⏸ pause
 app.post("/pause", (req, res) => {
-  current.playing = false;
+  playing = false;
   res.sendStatus(200);
 });
 
+// ▶ resume
+app.post("/resume", (req, res) => {
+  playing = true;
+  res.sendStatus(200);
+});
+
+// ⏭ next
 app.post("/next", (req, res) => {
-  current.url = "";
-  current.playing = false;
+  if (currentIndex < playlist.length - 1) {
+    currentIndex++;
+    playing = true;
+  }
   res.sendStatus(200);
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running on " + PORT));
+app.listen(PORT, () => console.log("Server running"));
